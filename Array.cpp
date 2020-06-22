768080ce7ce22515a7a3db3bb39b90a7123e3bc7
Ujwal K S <67190078+UjwalKSGeek@users.noreply.github.com>
2020-06-22 15:27:22 +0530

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,15 @@
+<include iostream>
+using namespace std;
+
+class Array(){
+private: int length;
+int size;
+int *A;
+public: Array(){
+size=10;
+length=0;
+A= new int[size];
+}
+Array(int S)
+{}
+};
